10023d649cf0dcf378cbdefd3280988281d0bbb6
AlperennK <34777135+AlperennK@users.noreply.github.com>
2019-11-06 17:46:24 +0100

    kafka consume produce

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # kafka-java-consproduce
 Kafka Java Consume
 
+#Requirements:
+To connect Kafka Broker
+Use Kafka Docker Image or install locally with following guide.
+
+https://hub.docker.com/r/kafkamanager/kafka-manager
+https://dzone.com/articles/kafka-avro-serialization-and-the-schema-registry
+
+
 To consume:
-Comment out consumeKafka() method in main function.
+Use consumeKafka() to consume
 
 To run:
-gradlew run
+gradlew run will generate inventory 
+
+To be implemented:
+-Write apache avro schemas
+-Write tests to verify/validate different avro schemes
--- src/main/java/com/conproducekafka/ccdak/producer/Main.java
+++ src/main/java/com/conproducekafka/ccdak/producer/Main.java
@@ -1,34 +1,29 @@
-package com.linuxacademy.ccdak.consumer;
-
-import java.io.*;
+package com.conproducekafka.ccdak.producer;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
-import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import java.util.Properties;
-
 public class Main {
-    public static Properties setProperties(){
+    public static void produceKafka(){
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
         props.put("acks", "all");
-        return props;
-    }
-
-    public static void produceToKafka() {
-
-        Properties props=setProperties();
 
         Producer<String, String> producer = new KafkaProducer<>(props);
+
         try {
             File file = new File(Main.class.getClassLoader().getResource("sample_transaction_log.txt").getFile());
             BufferedReader br = new BufferedReader(new FileReader(file));
@@ -41,54 +36,38 @@ public class Main {
                 if (key.equals("apples")) {
                     producer.send(new ProducerRecord<>("apple_purchases", key, value));
                 }
-
-
             }
             br.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+
         producer.close();
 
 
     }
-
-    public static void consumeKafka() {
+    public static void consumeKafka(){
         Properties props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9092");
-        props.setProperty("group.id", "group1");
+        props.setProperty("group.id", "my-group");
         props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("inventory_purchases"));
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("output.dat", true));
-            while (true) {
-                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-                for (ConsumerRecord<String, String> record : records) {
-                    String recordString = "key=" + record.key() + ", value=" + record.value() + ", topic=" + record.topic() + ", partition=" + record.partition() + ", offset=" + record.offset();
-                    System.out.println(recordString);
-                    writer.write(recordString + "\n");
-                }
-                consumer.commitSync();
-                writer.flush();
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList("purchase"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-
-
         }
     }
 
+
     public static void main(String[] args) {
-        produceToKafka();
+        produceKafka();
         //consumeKafka();
 
-
-    }
 }
-
+}
